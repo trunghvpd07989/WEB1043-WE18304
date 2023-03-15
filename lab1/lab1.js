82c33e708d4ec9e1208b1843ed55ef64049d2e9d
unknown <trunghvpd07989@fpt.edu.vn>
2023-03-15 18:03:10 +0700

    upload

---
+++ lab1/lab1.js
@@ -0,0 +1,10 @@
+// Yêu cầu người dùng nhập 2 số nguyên a và b từ bàn phím
+let a = parseInt(prompt("Nhập số nguyên a: "));
+let b = parseInt(prompt("Nhập số nguyên b: "));
+
+// Tính tổng của a và b
+let c = a + b;
+
+// In ra kết quả bằng phương thức document.write và console.log
+document.write("Tổng của " + a + " và " + b + " là " + c);
+console.log("Tổng của " + a + " và " + b + " là  " + c);
